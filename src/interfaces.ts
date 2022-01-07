662f4a5bcf18a4c3d1fcda0081e3ea17c36f6be6
Ross Keenan <rossk29@gmail.com>
2022-01-07 11:39:50 +0200

    feat: :label: Better types

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,7 @@
 export interface Settings {
 	fileColumnName: string;
-    listDelimiter: string;
+	listDelimiter: string;
 }
+
+export type Cell = string | Record<string, any>;
+export type Row = Record<string, Cell>;
