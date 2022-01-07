55d39d9e92f5291e7cc6ad0d8b86de5218fff77d
Ross Keenan <rossk29@gmail.com>
2022-01-07 10:55:24 +0200

    feat: :sparkles: parseCSV using great lib

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Link } from "obsidian-dataview";
 import { DEFAULT_SETTINGS } from "./const";
 import { Settings } from "./interfaces";
 import { SettingTab } from "./SettingTab";
+import helper from "csvtojson";
 
 export default class ImportPlugin extends Plugin {
 	settings: Settings;
@@ -13,7 +14,16 @@ export default class ImportPlugin extends Plugin {
 
 	onunload() {}
 
+	async parseCSV(
+		file: File
+	): Promise<
 		Record<string, string | number | string[] | number[] | DateTime | Link>
+	> {
+		const csv = await file.text();
+		const parser = helper();
+
+		return await parser.fromString(csv).then((json) => json);
+	}
 	async loadSettings() {
 		this.settings = Object.assign(
 			{},
